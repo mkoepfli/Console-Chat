7bf101a55b8d725b67fe6e9181d86401960406c1
Marco <mkoepfli@hosttech.ch>
2020-10-31 21:55:19 +0100

    added jUnit

--- src/net/codejava/networking/chat/client/WriteThread.java
+++ src/net/codejava/networking/chat/client/WriteThread.java
@@ -14,6 +14,7 @@ public class WriteThread extends Thread {
         String userName = console.readLine("\nGib deinen Username ein: ");
         client.setUserName(userName);
         writer.println(userName);
+
         String text;
 
         do {
